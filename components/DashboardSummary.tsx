ff53e26aded5f5e7eba379df20c073bc57275906
K.Kartheepan <e21214@eng.pdn.ac.lk>
2026-04-27 09:17:16 +0530

    Add DashboardSummary component

---
+++ components/DashboardSummary.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardSummary: React.FC = () => {
+    return (
+        <div>
+            <h1>Dashboard Summary</h1>
+            {/* Add more summary details here */}
+        </div>
+    );
+};
+
+export default DashboardSummary;
